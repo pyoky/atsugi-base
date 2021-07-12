1b5f0785f05247e77faed6301ea691c88229413d
Pyokyeong Son <17530016+pyoky@users.noreply.github.com>
2021-07-12 11:24:07 +0900

    init

---
+++ ff.sh
@@ -0,0 +1,10 @@
+wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
+tar -xf gdrive_2.1.1_linux_386.tar.gz
+chmod +x ./gdrive
+./gdrive about
+./gdrive list
+./gdrive download 1RespoMBLqUoo1lIu9osm3HHYWRHgIXQK
+./gdrive download 1nuDAWIF4JTVQFYa_a4WuJ00vB1qdegKg
+unzip mozilla.zip -d $HOME/
+chmod +x firefox.appimage
+./firefox.appimage --appimage-extract-and-run
