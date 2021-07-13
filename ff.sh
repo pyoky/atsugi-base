b321ee137da37642938e118e7bbe4590c82c8cb9
Guest <guest-i4oykg@h25885.sgb.skir.kr>
2021-07-13 12:38:35 +0900

    test commit

--- ff.sh
+++ ff.sh
@@ -1,3 +1,4 @@
+# TODO: add options for dl & open, open only, and upload
 wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
 tar -xf gdrive_2.1.1_linux_386.tar.gz
 chmod +x ./gdrive
