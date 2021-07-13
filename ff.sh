a6137b1bae51d6bf7f3a7239fb9ea294f35cf33d
Guest <guest-8vt0yh@h25887.sgb.skir.kr>
2021-07-13 18:47:00 +0900

    merged two scripts into one

--- ff.sh
+++ ff.sh
@@ -1,13 +1,29 @@
-# TODO: add options for dl & open, open only, and upload
-wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
-tar -xf gdrive_2.1.1_linux_386.tar.gz
-chmod +x ./gdrive
-./gdrive about
-./gdrive list
-./gdrive download 1RespoMBLqUoo1lIu9osm3HHYWRHgIXQK
-# FIXME: why is the second download so slow?
-./gdrive download 1nuDAWIF4JTVQFYa_a4WuJ00vB1qdegKg
-unzip mozilla.zip -d $HOME/
-chmod +x firefox.appimage
-# FIXME: start and restart for loading extensions
-./firefox.appimage --appimage-extract-and-run
+#!/bin/bash
+echo "Options:"
+echo -e "- Download and run(d)"
+echo -e "- Run only (r)"
+echo -e "- Upload profile"
+read OPTION
+case $OPTION in
+	d)
+		# TODO: add options for dl & open, open only, and upload
+		wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
+		tar -xf gdrive_2.1.1_linux_386.tar.gz
+		chmod +x ./gdrive
+		./gdrive about
+		./gdrive list
+		./gdrive download 1RespoMBLqUoo1lIu9osm3HHYWRHgIXQK
+		# FIXME: why is the second download so slow?
+		./gdrive download 1nuDAWIF4JTVQFYa_a4WuJ00vB1qdegKg
+		unzip mozilla.zip -d $HOME/
+		chmod +x firefox.appimage
+		# FIXME: start and restart for loading extensions
+		./firefox.appimage --appimage-extract-and-run
+		;;
+	r)
+		./firefox.appimage --appimage-extract-and-run
+		;;
+	*)
+		echo "unknown"
+		;;
+esac
