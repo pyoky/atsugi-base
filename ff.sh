cca7813556490406b290c7d7bf8234ebd85e0c4a
Guest <guest-fpo1ew@h25887.sgb.skir.kr>
2021-07-21 15:06:35 +0900

    added auto browser open

--- ff.sh
+++ ff.sh
@@ -12,7 +12,7 @@ case $OPTION in
 		wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
 		tar -xf gdrive_2.1.1_linux_386.tar.gz
 		chmod +x ./gdrive
-		./gdrive about
+    ./gdrive about | tee >(egrep -o -m 1 'https?://[^ ]+' | xargs xdg-open > /dev/null 2>&1)
 		./gdrive list
 		./gdrive download 1RespoMBLqUoo1lIu9osm3HHYWRHgIXQK
 		# FIXME: why is the second download so slow?
